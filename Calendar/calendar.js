444e1de45e0a554bb11f62ec7802d4f409464c4e
wolfwows <wolfwows@gmail.com>
2018-08-28 17:16:56 +0800

    添加点击签到事件

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -18,46 +18,74 @@ var Calendar = Class.create();
 Calendar.prototype = {
   initialize: function(calendarBox, options) {
     this.calendarBox = $$(calendarBox);
-    this.Days = [];
+    // this.Days = [];
     this.setOptions(options);
     this.Year = this.options.Year;
     this.Month = this.options.Month;
+    this.Today = this.options.Today;
+    this.signDay = this.options.signDay;
     this.draw();
   },
   setOptions: function(options) {
     this.options = {
       Year: new Date().getFullYear(),
       Month: new Date().getMonth() + 1,
+      Today: new Date().getDate(),
+      signDay: null
     };
     Object.extend(this.options, options || {});
   },
+  // 绘制日历表的方法
   draw: function() {
-    var arr = [];
+    var days = [];
     // getDay()函数返回当前月第一天所在的星期数
     for (var i = 1, firstDay = new Date(this.Year, this.Month - 1, 1).getDay(); i <= firstDay; i++) {
       // 通过push()函数将空值传入数组
-      arr.push("&nbsp;");
+      days.push("&nbsp;");
     }
     // 通过getDate()函数获取当前月的天数
     for (var i = 1, monthDay = new Date(this.Year, this.Month, 0).getDate(); i <= monthDay; i++) {
       // 通过push()函数将当前月的天数传入数组
-      arr.push(i);
+      days.push(i);
     }
     var frag = document.createDocumentFragment();
     this.Days = [];
-    while (arr.length > 0) {
+    while (days.length > 0) {
       var row = document.createElement("tr");
       for (var i = 1; i <= 7; i++) {
         var cell = document.createElement("td");
         cell.innerHTML = "&nbsp;";
-        if (arr.length > 0) {
-          var d = arr.shift();
-          cell.innerHTML = "<span>" + d + "</span>";
+        if (days.length > 0) {
+          var day = days.shift();
+          cell.innerHTML = "<span>" + day + "</span>";
+          // 为当天的日期添加样式
+          if(day == this.Today){
+            cell.classList.add("onToday");
+          }
+          if(day > 0 && )
         }
         row.appendChild(cell);
       }
       frag.appendChild(row);
     }
     this.calendarBox.appendChild(frag);
+  },
+  signIn:function(){
+    var now = new Date();
+		var Year = now.getFullYear();
+		var Month = now.getMonth() + 1;
+		if(Year != this.Year || Month != this.Month){
+			this.Year = Year;
+			this.Month = Month;
+			this.isSingIn = true;
+			return this.Draw();
+		}
+    document.getElementsByClassName("onToday")[0].classList.add("onSign");
+		this.signDay.push(Date.parse(new Date()) / 1000);
+//					此处应当提交修改后的签到数组
+		console.log(this.signDay);
+
   }
+
+
 }
