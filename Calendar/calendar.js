b740ae3c915b73ad92182b04c4e76e0c72f7e57d
wolfwows <wolfwows@gmail.com>
2018-08-28 18:01:07 +0800

    筛查签到记录数组中的日期，在当前显示月份内如果存在则更改相应的日期样式

--- Calendar/calendar.js
+++ Calendar/calendar.js
@@ -23,7 +23,7 @@ Calendar.prototype = {
     this.Year = this.options.Year;
     this.Month = this.options.Month;
     this.Today = this.options.Today;
-    this.signDay = this.options.signDay;
+    this.signDays = this.options.signDays;
     this.draw();
   },
   setOptions: function(options) {
@@ -31,13 +31,14 @@ Calendar.prototype = {
       Year: new Date().getFullYear(),
       Month: new Date().getMonth() + 1,
       Today: new Date().getDate(),
-      signDay: null
+      signDays: []
     };
     Object.extend(this.options, options || {});
   },
   // 绘制日历表的方法
   draw: function() {
     var days = [];
+    var signRecodes = this.signDays;
     // getDay()函数返回当前月第一天所在的星期数
     for (var i = 1, firstDay = new Date(this.Year, this.Month - 1, 1).getDay(); i <= firstDay; i++) {
       // 通过push()函数将空值传入数组
@@ -62,7 +63,14 @@ Calendar.prototype = {
           if(day == this.Today){
             cell.classList.add("onToday");
           }
-          if(day > 0 && )
+          // 在此处筛查签到记录数组中的日期，并赋予样式
+          if(day > 0 && signRecodes.length){
+            for(let i = 0; i < signRecodes.length; i++){
+              if(this.isSame(new Date(this.Year,this.Month - 1, day),signRecode[i])){
+								cell.classList.add("onSign");
+							}
+            }
+          }
         }
         row.appendChild(cell);
       }
@@ -70,6 +78,11 @@ Calendar.prototype = {
     }
     this.calendarBox.appendChild(frag);
   },
+  // 此函数进行当前月份的日期与签到记录数组里的日期是否相等
+  isSame: function(d1, d2){
+		d2 = new Date(d2 * 1000);
+		return(d1.getFullYear() == d2.getFullYear() && d1.getMonth() == d2.getMonth() && d1.getDate() == d2.getDate());
+	},
   signIn:function(){
     var now = new Date();
 		var Year = now.getFullYear();
@@ -81,10 +94,9 @@ Calendar.prototype = {
 			return this.Draw();
 		}
     document.getElementsByClassName("onToday")[0].classList.add("onSign");
-		this.signDay.push(Date.parse(new Date()) / 1000);
+		this.signDays.push(Date.parse(new Date()) / 1000);
 //					此处应当提交修改后的签到数组
-		console.log(this.signDay);
-
+		console.log(this.signDays);
   }
 
 
